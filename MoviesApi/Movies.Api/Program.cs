ccdbe6ee4df87508126b671f56bb467112f5447b
Felix Wedel <wedelfelix@hotmail.ca>
2025-03-10 22:13:38 -0500

    Register our Db in program.cs and add scaler to generate api docs in Dev

--- MoviesApi/Movies.Api/Program.cs
+++ MoviesApi/Movies.Api/Program.cs
@@ -1,20 +1,35 @@
+using Movies.Api.Mapping;
 using Movies.Application;
+using Movies.Application.Database;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
 
 builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]!);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+        options.WithTheme(ScalarTheme.DeepSpace).Servers = []);
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseMiddleware<ValidationMappingMiddleware>();
+app.MapControllers();
+
+var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await dbInitializer.InitializeAsync();
 
 app.Run();
