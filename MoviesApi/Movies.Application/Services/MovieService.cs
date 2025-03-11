cf09bc9a204f6dbf645bdf5d6378f02ff1034e37
Felix Wedel <wedelfelix@hotmail.ca>
2025-03-10 22:02:45 -0500

    Implemented and registered new MovieService as well as validation and validation handleing middleware and validation response contracts

---
+++ MoviesApi/Movies.Application/Services/MovieService.cs
@@ -0,0 +1,48 @@
+using FluentValidation;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+
+namespace Movies.Application.Services;
+
+public class MovieService(IMovieRepository movieRepository, IValidator<Movie> movieValidator) : IMovieService
+{
+    public async Task<bool> CreateAsync(Movie movie, CancellationToken cancellationToken = default)
+    {
+        await movieValidator.ValidateAndThrowAsync(movie, cancellationToken);
+        return await movieRepository.CreateAsync(movie, cancellationToken);
+    }
+
+    public Task<Movie?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return movieRepository.GetByIdAsync(id, cancellationToken));
+    }
+
+    public Task<Movie?> GetBySlugAsync(string slug, CancellationToken cancellationToken = default)
+    {
+        return movieRepository.GetBySlugAsync(slug, cancellationToken));
+    }
+
+    public Task<IEnumerable<Movie>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return movieRepository.GetAllAsync(cancellationToken));
+    }
+
+    public async Task<Movie?> UpdateAsync(Movie movie, CancellationToken cancellationToken = default)
+    {
+        await movieValidator.ValidateAndThrowAsync(movie, cancellationToken);
+        var movieExists = await movieRepository.ExistsByIdAsync(movie.Id, cancellationToken));
+        if (!movieExists) return null;
+        await movieRepository.UpdateAsync(movie, cancellationToken));
+        return movie;
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return movieRepository.DeleteByIdAsync(id, cancellationToken));
+    }
+
+    public Task<bool> ExistsByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return movieRepository.ExistsByIdAsync(id, cancellationToken));
+    }
+}
